7c66db7c3531f315c7a1faa1fd851910c7a893b5
gus <gusquinalha@gmail.com>
2025-05-15 14:19:13 -0300

    ADD: ajustes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,8 @@
+// tailwind.config.mjs
+
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -5,6 +10,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [require('tailwindcss'),
-	require('autoprefixer'),],
+	plugins: [tailwindcss, autoprefixer],
 }
